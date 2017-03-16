8b6c10095c8c2305840edac9b986249c34c758a9
Jean Paul Ruiz <jpruiz114@gmail.com>
2017-03-16 16:23:02 -0400

    Adding version

---
+++ tests/GeoNamesOrgParserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+/**
+ * Class GeoNamesOrgParserTest
+ */
+class GeoNamesOrgParserTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testTrueIsTrue()
+    {
+        $foo = true;
+        $this->assertTrue($foo);
+    }
+}
