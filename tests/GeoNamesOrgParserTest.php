5f3f8eb020c21fc6c675dd926260473d43c900c9
Jean Paul Ruiz <jpruiz114@gmail.com>
2017-03-16 17:08:12 -0400

    Adding unit tests

--- tests/GeoNamesOrgParserTest.php
+++ tests/GeoNamesOrgParserTest.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use TheDMSGrp\ZipGeo\GeoNamesOrgParser;
+
 /**
  * Class GeoNamesOrgParserTest
  */
@@ -10,9 +12,53 @@ class GeoNamesOrgParserTest extends PHPUnit_Framework_TestCase
     /**
      *
      */
-    public function testTrueIsTrue()
+    public function testGetInfoByCityAndState()
     {
-        $foo = true;
-        $this->assertTrue($foo);
+        $city = "Clearwater";
+        $state = "FL";
+
+        $result = GeoNamesOrgParser::getGeoInfoByCityAndStateName($city, $state);
+
+        $this->assertNotEmpty($result);
+
+        $this->assertArrayHasKey('country', $result);
+        $this->assertArrayHasKey('zipcode', $result);
+        $this->assertArrayHasKey('city', $result);
+        $this->assertArrayHasKey('state-name', $result);
+        $this->assertArrayHasKey('state-code', $result);
+        $this->assertArrayHasKey('county', $result);
+
+        $this->assertEquals($result['country'], 'US');
+        $this->assertEquals($result['zipcode'], '33755');
+        $this->assertEquals($result['city'], 'CLEARWATER');
+        $this->assertEquals($result['state-name'], 'FLORIDA');
+        $this->assertEquals($result['state-code'], 'FL');
+        $this->assertEquals($result['county'], 'PINELLAS');
+    }
+
+    /**
+     *
+     */
+    public function testGetInfoByCity()
+    {
+        $city = "Clearwater";
+
+        $result = GeoNamesOrgParser::getGeoInfoByCityAndStateName($city, null);
+
+        $this->assertNotEmpty($result);
+
+        $this->assertArrayHasKey('country', $result);
+        $this->assertArrayHasKey('zipcode', $result);
+        $this->assertArrayHasKey('city', $result);
+        $this->assertArrayHasKey('state-name', $result);
+        $this->assertArrayHasKey('state-code', $result);
+        $this->assertArrayHasKey('county', $result);
+
+        $this->assertEquals($result['country'], 'US');
+        $this->assertEquals($result['zipcode'], '33755');
+        $this->assertEquals($result['city'], 'CLEARWATER');
+        $this->assertEquals($result['state-name'], 'FLORIDA');
+        $this->assertEquals($result['state-code'], 'FL');
+        $this->assertEquals($result['county'], 'PINELLAS');
     }
 }
